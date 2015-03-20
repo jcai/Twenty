63a1ad6ec87e189d64d76c233c498c9aad9dc323
Jun Tsai <jcai@ganshane.com>
2015-03-20 15:24:48 +0800

    初始化导入

---
+++ app/src/main/java/cn/manmanwu/twenty/MainActivity.java
@@ -0,0 +1,60 @@
+package cn.manmanwu.twenty;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setFullscreen();
+
+        setContentView(R.layout.activity_main);
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage("正在加载，请稍候...");
+        dialog.setIndeterminate(true);
+        dialog.setCancelable(true);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedError(WebView view, int errorCode,
+                                        String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, "漫漫屋");
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                dialog.setProgress(100);
+                dialog.hide();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                dialog.setProgress(0);
+                dialog.show();
+            }
+        });
+        webView.loadUrl("http://www.manmanwu.cn/appgh/index.php");
+    }
+
+
+    public void setFullscreen() {
+        getWindow().clearFlags(
+                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);// 清除FLAG
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
